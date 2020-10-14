c864970b075e239d8b3fc2a78a11ba5ff94c35a1
Alan Pinho <alan_qu_pinho@yahoo.com.br>
2020-10-14 18:20:55 -0300

    Alterações nas entradas de dados

--- avaliacao.py
+++ avaliacao.py
@@ -1,11 +1,9 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 #Efetua a leitura do CSV de apenas 1000 entradas e de determinadas colunas
-df = pd.read_csv('Base-INFLUD-14-09-2020.csv', sep=';', nrows=1000,  usecols = ['SG_UF_NOT','FEBRE','TOSSE','GARGANTA', 
-                 'DISPNEIA', 'DESC_RESP', 'SATURACAO', 'FATOR_RISC', 'CLASSI_FIN', 'EVOLUCAO'])
+df = pd.read_csv('Base-INFLUD-14-09-2020.csv', sep=';', nrows=1000,  usecols = ['SG_UF_NOT','CLASSI_FIN', 'EVOLUCAO'])
 
 
 #Extrai da base de dados apenas os estados das 1000 entradas
@@ -35,9 +33,7 @@ def count_pessoas():
 def novo_dataframe(dados):
 
     #definir novo dataframe organizando as pessoas por estado
-    new_df= pd.DataFrame(columns=['Pessoas', 'Febre', 'Tosse', 'Dor de Garganta', 'Falta de ar (Dispneia)', 
-                     'Desconforto Respiratório', 'Saturação O²< 95%', 'Fator de Risco', 'COVID-19', 
-                     'Outras doenças', 'Óbito por COVID-19'], index=uf_estados)
+    new_df= pd.DataFrame(columns=['Pessoas', 'Óbito por COVID-19'], index=uf_estados)
     
     return new_df
 
@@ -53,67 +49,19 @@ def add_pessoa_por_estado():
 #Adciona no novo DataFrame, as demais informações escolhidas para analise, das pessoas em cada estado do país
 def add_dados():
 
-    ct_febre =[]
-    ct_tosse = []
-    ct_garganta = []
-    ct_dispneia = []
-    ct_desc_resp = []
-    ct_saturacao = []
-    ct_fator_risc = []
-    ct_classi_fin = []
-    ct_outr_doec = []
     ct_obtb_covid = []
-    total =[]
-    ct_f = ct_t = ct_g = ct_d = ct_df = ct_s = ct_fr = ct_cf = ct_od = ct_oc = 0
+    ct_oc = 0
 
     for uf in uf_estados:
         for index, row in df.iterrows():
             if row['SG_UF_NOT'] == uf: 
-                if row['FEBRE'] == 1:
-                    ct_f+=1
-                if row['TOSSE'] == 1:
-                    ct_t+=1
-                if row['GARGANTA'] == 1:
-                    ct_g+=1
-                if row['DISPNEIA'] == 1:
-                    ct_d+=1
-                if row['DESC_RESP'] == 1:
-                    ct_df+=1
-                if row['SATURACAO'] == 1:
-                    ct_s+=1
-                if row['FATOR_RISC'] == 'S':
-                    ct_fr+=1
-                if row['CLASSI_FIN'] == 5:
-                    ct_cf+=1
-                else:
-                    ct_od+=1
                 if row['CLASSI_FIN'] == 5 and row['EVOLUCAO'] == 2:
                     ct_oc+=1              
 
-        ct_febre.append(ct_f)
-        ct_tosse.append(ct_t)
-        ct_garganta.append(ct_g)
-        ct_dispneia.append(ct_d)
-        ct_desc_resp.append(ct_df)
-        ct_saturacao.append(ct_s)
-        ct_fator_risc.append(ct_fr)
-        ct_classi_fin.append(ct_cf)
-        ct_outr_doec.append(ct_od)
         ct_obtb_covid.append(ct_oc)
-        ct_f = ct_t = ct_g = ct_d = ct_df = ct_s = ct_fr = ct_cf = ct_od = ct_oc = 0
+        ct_oc = 0
 
-    new_df['Febre'] = ct_febre
-    new_df['Tosse'] = ct_tosse
-    new_df['Dor de Garganta'] = ct_garganta
-    new_df['Falta de ar (Dispneia)'] = ct_dispneia
-    new_df['Desconforto Respiratório'] = ct_desc_resp
-    new_df['Saturação O²< 95%'] = ct_saturacao
-    new_df['Fator de Risco'] = ct_fator_risc
-    new_df['COVID-19'] = ct_classi_fin
-    new_df['Outras doenças'] = ct_outr_doec
     new_df['Óbito por COVID-19'] = ct_obtb_covid
-
-    #print(new_df)
 
 
 #Realiza a normalização dados obtidos no novo DataFrame
